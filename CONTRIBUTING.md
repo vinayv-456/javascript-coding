dad7a469ea8306c1c6da057cc8bf53321c4ee052
vinayv-456 <vinayvallabineni456@gmail.com>
2022-05-17 11:48:19 +0530

    Add promises module

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+References:
+javascript Basic: https://www.jschallenger.com/
+Promises: https://github.com/atapas/promise-interview-ready
---
+++ Promises/01-callbacks/main.js
@@ -0,0 +1,26 @@
+// <<<<<<<<<<<Excercise-1>>>>>>>>>>
+console.log("Callback");
+
+function foo(bar) {
+  bar();
+}
+
+foo(function () {
+  console.log("bar");
+});
+
+// <<<<<<<<<<<<Excercise-2>>>>>>>>>>
+// Pizza Hub
+function orderPizza(type, name, callback) {
+  console.log("Ordering pizza...");
+  setTimeout(function () {
+    let msg = `Your ${type} ${name} Pizza is ready! The total bill is $13`;
+    console.log(`On the Pizzahub server ${msg}`);
+    callback(msg);
+  }, 3000);
+}
+
+// Robin's Phone
+orderPizza("veg", "cheese", function (msg) {
+  console.log(`On Robin's phone ${msg}`);
+});
